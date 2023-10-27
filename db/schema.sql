caf28934c82b0fb9e8c5a035dcad2339f441aa4b
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-10-26 18:25:17 -0700

    Fixed schema.sql syntax

--- db/schema.sql
+++ db/schema.sql
@@ -3,26 +3,23 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE departments(
-    id INT,
-    name VARCHAR(30),
-    PRIMARY KEY (id)
+CREATE TABLE departments (
+  id INT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id: INT PRIMARY KEY,
-    title: VARCHAR(30),
-    salary: DECIMAL,
-    department_id: INT,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-)
+  id INT PRIMARY KEY, 
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
 
 CREATE TABLE employee (
-    id: INT,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-)
+  id INT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30), 
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id)  
+);
