87289d5eaf1dbd46a0ee007aef9b07e444a5490d
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2023-11-14 18:29:23 -0800

    Added the ability to add an employee to the database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
 USE employee_db;
 
-INSERT INTO departments(id, name)
-VALUES  (001, 'Sales'),
-        (002, 'Legal'),
-        (003, 'Engineering');
+INSERT INTO departments(name)
+VALUES  ('Sales'),
+        ('Legal'),
+        ('Engineering');
 
-INSERT INTO role(id, title, salary, department_id)
-VALUES  (001, 'Associate', 120000.00, 001),
-        (002, 'Paralegal', 60000.00, 002),
-        (003, 'Project Manager', 150000, 003);
+INSERT INTO role(title, salary, department_id)
+VALUES  ('Associate', 120000.00, 001),
+        ('Paralegal', 60000.00, 002),
+        ('Project Manager', 150000, 003);
 
-INSERT INTO employee (id, first_name, last_name, role_id)
-VALUES  (001, 'Jack', 'Lund', '001'), 
-        (002, 'Jenna', 'Mable', '002'), 
-        (003, 'Katy', 'Hemsworth', '003');
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES  ('Jack', 'Lund', '001'), 
+        ('Jenna', 'Mable', '002'), 
+        ('Katy', 'Hemsworth', '003');
 
--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 const db = require('./db/connection');
+const inquirer = require('inquirer');
 
 function viewAllEmployees(callback) {
     // Implement logic for viewing all employees
@@ -11,6 +12,35 @@ function viewAllEmployees(callback) {
 
 function addEmployee(callback) {
     // Implement logic for adding an employee
+    // An employee needs a first name and a last name and role id
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is the first name of the emplyee you would like to add?',
+                name: 'newEmployeeFirst'
+            },
+            {
+                type: 'input',
+                message: 'What is the last name of the employee you would like to add',
+                name: 'newEmployeeLast'
+            },
+            {
+                type: 'input',
+                message: 'Please pick the number (1-9) that coorisponds with your new employees role',
+                name: 'newEmployeeRole'
+            }
+        ]).then((data) => {
+            const newFirst = data.newEmployeeFirst;
+            const newLast = data.newEmployeeLast;
+            const newRole = data.newEmployeeRole;
+            db.query("INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)", [newFirst, newLast, newRole],
+            function (){
+                //console.log(results);
+                console.log('New Employee Added');
+                callback();
+            });
+        }).catch((err) => console.log(err));
 }
 
 function updateEmployeeRole(callback) {
